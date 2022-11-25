dbd9dbbfcc82823a2693dede7bac1a94b6249a18
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-25 15:38:01 -0600

    Define test for empty event

--- lambdas/test_all.py
+++ lambdas/test_all.py
@@ -1,12 +1,35 @@
-from check_code_form.lambda_function import check_code_form
-from check_code_used.lambda_function import check_code_used
-from generate_link.lambda_function import generate_link
+from check_code_form.lambda_function import check_code_form, lambda_handler as check_code_form_lambda_handler
+from check_code_used.lambda_function import check_code_used, lambda_handler as check_code_used_lambda_handler
+from generate_link.lambda_function import generate_link, lambda_handler as generate_link_lambda_handler
+
+from json import loads as json_loads
+from pytest import fixture, mark
+
+with open('events.json', 'r') as events_file:
+    EVENTS = json_loads(events_file.read())
+
+
+@fixture
+def events():
+    return EVENTS
+
+
+@fixture
+def context():
+    return object()
 
 
 def test_check_code_form():
     assert 'abcde-12345-fghij' == check_code_form('abcde-12345-fghij')
     assert 'abcde-12345-fghij' == check_code_form('ABCDE-12345-FGHIJ')
     assert '' == check_code_form('test')
+
+
+@mark.parametrize('event_name', ['empty'])
+def test_check_code_form_lambda_handler(events, event_name, context):
+    event = events[event_name]['event']
+    response = check_code_form_lambda_handler(event, context)
+    assert response == events[event_name]['response']
 
 
 def test_check_code_used():
