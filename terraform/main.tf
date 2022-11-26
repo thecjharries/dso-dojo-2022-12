11ef43aba75f5ad04fd7ba75bd22404815be1aaa
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-25 20:01:02 -0600

    Add all Lambdas

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,78 @@ resource "null_resource" "zip" {
     command = "cd ${path.module}/../lambdas && make clean && make all"
   }
 }
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name               = "dso-dojo-2022-12-lambda-basic"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+locals {
+  lambdas = toset([
+    "check_code_form",
+    "check_code_used",
+    "generate_link",
+  ])
+}
+
+resource "aws_lambda_function" "basic_lambdas" {
+  for_each = local.lambdas
+
+  filename      = "${path.module}/../lambdas/${each.key}.zip"
+  function_name = each.key
+  role          = aws_iam_role.lambda.arn
+  runtime       = "python3.9"
+  handler       = "lambda_function.lambda_handler"
+}
+
+data "aws_iam_policy_document" "lambda_invoke" {
+  statement {
+    actions = [
+      "lambda:InvokeFunction",
+      "lambda:InvokeAsync",
+    ]
+
+    resources = [
+      for lambda in aws_lambda_function.basic_lambdas : lambda.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_invoke" {
+  name   = "dso-dojo-2022-12-lambda-basic-invoke"
+  policy = data.aws_iam_policy_document.lambda_invoke.json
+}
+
+resource "aws_iam_role" "lambda_invoke" {
+  name               = "dso-dojo-2022-12-lambda-basic-invoke"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_invoke" {
+  role       = aws_iam_role.lambda_invoke.name
+  policy_arn = aws_iam_policy.lambda_invoke.arn
+}
+
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "runner" {
+  filename      = "${path.module}/../lambdas/runner.zip"
+  function_name = "runner"
+  role          = aws_iam_role.lambda_invoke.arn
+  runtime       = "python3.9"
+  handler       = "lambda_function.lambda_handler"
+}
